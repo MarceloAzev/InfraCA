597ccf963665c4835ee2536319d218ee393dba0a
MarceloAzev <marcelo.azevedo@gmail.com>
2023-10-31 16:22:19 -0300

    organização de pasta

---
+++ env/Desenvolvimento/main.tf
@@ -0,0 +1,12 @@
+
+module "aws-dev"{
+    source = "../../infra"
+    instancia_tipo = "t2.micro"
+    regiao_us_aws = "us-east-1"
+    chave = "iac-estudo"
+    amis = "ami-0fc5d935ebf8bc3bc"
+    instancia_name = "Dev01"
+}
+output "IP"{
+    value = module.aws-dev.IP_publico
+}
